3d479157e8b1dd64ec8cbde6c3c8f1c9ccc3b7da
Jaleel-VS <jeremikress@gmail.com>
2023-09-25 15:57:49 +0200

    update main file

--- hablemos.py
+++ hablemos.py
@@ -1,61 +1,85 @@
-from os import getenv
+import os
 from discord import Game, Intents
+import discord
 from discord.ext.commands import Bot, CommandNotFound, CommandOnCooldown
 from dotenv import load_dotenv
 
 load_dotenv('.env')
 
-PREFIX = "$"  # the real one, have to make it configurable some day
-# PREFIX = "-"  # for testing
 
-
-cog_extensions = ['cogs.convo_starter_cog',
-                  'cogs.general_cog',
-                  'cogs.hangman_cog',
-                  'cogs.quote_generator_cog',
-                  'cogs.reverso_cog']
 
 
 class Hablemos(Bot):
     error_channel = ""
     online_channel = ""
 
-    def __init__(self):
+    def __init__(self, prefix):
         super().__init__(description="Bot by Jaleel#6408",
-                         command_prefix=PREFIX,
+                         command_prefix=prefix,
                          owner_id=216848576549093376,
                          help_command=None,
-                         intents=Intents(members=True, messages=True, guilds=True)
+                         intents=discord.Intents.all()
                          )
 
-        for extension in cog_extensions:
-            self.load_extension(f'{extension}.main')
-            print(f"{extension} loaded")
+        self.online_channel = None
+        self.error_channel = None
+
+        
+    
+    async def setup_hook(self):
+        for filename in os.listdir("./cogs"):
+            if filename.endswith(".py"):
+                try:
+                    await self.load_extension(f"cogs.{filename[:-3]}")
+                    print(f"Loaded {filename}")
+                except Exception as e:
+                    print(f"Failed to load {filename}")
+                    print(f"[ERROR] {e}")
+
 
     async def on_ready(self):
         # error log in my personal server
-        self.error_channel = self.get_guild(731403448502845501).get_channel(811669166883995690)
-        self.online_channel = self.get_guild(731403448502845501).get_channel(808679873837137940)
+        guild_id = 731403448502845501
+        guild = self.get_guild(guild_id)
+
+        if guild is None:
+            print(f"Guild with ID {guild_id} not found")
+            return
+        self.error_channel = guild.get_channel(811669166883995690)
+        self.online_channel = guild.get_channel(808679873837137940)
         print("BOT LOADED!")
-        await self.online_channel.send("I'm online bra :smiling_imp:")
+
+        if isinstance(self.online_channel, discord.TextChannel):
+            await self.online_channel.send("I'm online bra :smiling_imp:")
+            
         await self.change_presence(activity=Game(f'{PREFIX}help'))
 
     async def on_command_error(self, ctx, error):
         if ctx.message.content[1].isdigit() or ctx.message.content[-1] == PREFIX:  # ignores dollar amounts and math bot
             return
         if isinstance(error, CommandNotFound):
-            await self.error_channel.send(
-                f"------\nCommand not found:\n{ctx.author}, {ctx.author.id}, {ctx.channel}, {ctx.channel.id}, "
-                f"{ctx.guild}, {ctx.guild.id}, \n{ctx.message.content}\n{ctx.message.jump_url}\n------")
+            if isinstance(self.error_channel, discord.TextChannel):
+                await self.error_channel.send(
+                    f"------\nCommand not found:\n{ctx.author}, {ctx.author.id}, {ctx.channel}, {ctx.channel.id}, "
+                    f"{ctx.guild}, {ctx.guild.id}, \n{ctx.message.content}\n{ctx.message.jump_url}\n------")
 
         if isinstance(error, CommandOnCooldown):
-            await ctx.send(f"This command is on cooldown.  Try again in {round(error.retry_after)} seconds.")
+            if isinstance(ctx.channel, discord.TextChannel):
+                await ctx.send(f"This command is on cooldown.  Try again in {round(error.retry_after)} seconds.")
 
     async def on_command_completion(self, ctx):
-        await self.error_channel.send(
-            f"------\nSuccessfully used by {ctx.author}, {ctx.channel},{ctx.guild}, "
-            f"{ctx.message.content}\n{ctx.message.jump_url}\n------")
+        if isinstance(self.error_channel, discord.TextChannel):
+            await self.error_channel.send(
+                f"------\nSuccessfully used by {ctx.author}, {ctx.channel},{ctx.guild}, "
+                f"{ctx.message.content}\n{ctx.message.jump_url}\n------")
 
 
-bot = Hablemos()
-bot.run(getenv('BOT_TOKEN'))
+PREFIX = os.getenv('PREFIX')
+bot = Hablemos(PREFIX)
+
+bot_token = os.getenv('BOT_TOKEN')
+
+if bot_token is None:
+    print("No token found")
+else:
+    bot.run(bot_token)
