cc971cfac6f09e0d144d7bc3ef524299595cdeb0
Jaleel-VS <jeremikress@gmail.com>
2024-03-09 17:32:35 +0200

    Refactor image_creator.py

--- cogs/quote_generator_cog/quote_generator_helper/image_creator.py
+++ cogs/quote_generator_cog/quote_generator_helper/image_creator.py
@@ -1,10 +1,7 @@
 from os import path
 import imgkit
-import time
-from PIL import Image
-import requests
 
-dir_path = path.dirname(path.dirname(path.realpath(__file__))).replace('\\', '/')
+dir_path = path.dirname(path.realpath(__file__))
 
 
 def create_image(user_name, user_avatar, message_content):
@@ -13,9 +10,6 @@ def create_image(user_name, user_avatar, message_content):
         'crop-w': '644',
         'encoding': "UTF-8",
         'enable-local-file-access': None,
-        'transparent': None,
-      
-
     }
 
     font_size = ""
@@ -37,23 +31,23 @@ def create_image(user_name, user_avatar, message_content):
             <style>   
         @font-face {{
         font-family: 'Satisfy Pro';
-        src: url('file:///{dir_path}/quote_generator_helper/fonts/SatisfyPro.eot');
-        src: url('file:///{dir_path}/quote_generator_helper/fonts/SatisfyPro.eot?#iefix') format('embedded-opentype'),
-             url('file:///{dir_path}/quote_generator_helper/fonts/SatisfyPro.woff') format('woff'),
-             url('file:///{dir_path}/quote_generator_helper/fonts/SatisfyPro.ttf') format('truetype'),
-             url('file:///{dir_path}/quote_generator_helper/fonts/SatisfyPro.svg#SatisfyPro') format('svg');
+        src: url('{dir_path}/fonts/SatisfyPro.eot');
+        src: url('{dir_path}/fonts/SatisfyPro.eot?#iefix') format('embedded-opentype'),
+             url('{dir_path}/fonts/SatisfyPro.woff') format('woff'),
+             url('{dir_path}/fonts/SatisfyPro.ttf') format('truetype'),
+             url('{dir_path}/fonts/SatisfyPro.svg#SatisfyPro') format('svg');
         font-weight: normal;
         font-style: normal;
         font-display: swap;
-        }}
-        
-        @font-face {{
+    }}
+    
+    @font-face {{
         font-family: 'Helvetica Neue';
-        src: url('file:///{dir_path}/quote_generator_helper/fonts/HelveticaNeue-Roman.eot');
-        src: url('file:///{dir_path}/quote_generator_helper/fonts/HelveticaNeue-Roman.eot?#iefix') format('embedded-opentype'),
-             url('file:///{dir_path}/quote_generator_helper/fonts/HelveticaNeue-Roman.woff') format('woff'),
-             url('file:///{dir_path}/quote_generator_helper/fonts/HelveticaNeue-Roman.ttf') format('truetype'),
-             url('file:///{dir_path}/quote_generator_helper/fonts/HelveticaNeue-Roman.svg#HelveticaNeue-Roman') format('svg');
+        src: url('{dir_path}/fonts/HelveticaNeue-Roman.eot');
+        src: url('{dir_path}/fonts/HelveticaNeue-Roman.eot?#iefix') format('embedded-opentype'),
+             url('{dir_path}/fonts/HelveticaNeue-Roman.woff') format('woff'),
+             url('{dir_path}/fonts/HelveticaNeue-Roman.ttf') format('truetype'),
+             url('{dir_path}/fonts/HelveticaNeue-Roman.svg#HelveticaNeue-Roman') format('svg');
         font-weight: normal;
         font-style: normal;
         font-display: swap;
@@ -65,7 +59,8 @@ def create_image(user_name, user_avatar, message_content):
     width: 634px;
     height: 256px;
 
- 
+    -webkit-filter: grayscale(100%);
+    filter: grayscale(100%);
     }}
     
         .myImage {{
@@ -76,12 +71,9 @@ def create_image(user_name, user_avatar, message_content):
             border-bottom-left-radius: 8px;
             width: 258px;
             height: 256px;
-            
-            -webkit-filter: grayscale(100%);
-            filter: grayscale(100%);
             }}
-        
-        
+
+
             .quote {{
                 float: left;
                 width: 376px;
@@ -93,33 +85,30 @@ def create_image(user_name, user_avatar, message_content):
                 border-top-right-radius: 8px;
                 border-bottom-right-radius: 8px;
 
-                word-wrap: break-word;
-                hyphens: auto;
-
                 text-align: center;
                 }}
-        
+
             .main-quote {{
             color: white;
             font-family: Helvetica Neue;
             font-size: {font_size};
             padding: 10% 5% 5%;
             }}
-        
+
             
-        
+
             .author {{
                 color: white;
                 font-size: 135%;
                 font-family: Satisfy Pro;
             }}
-        
+
             span:after,
         span:before{{
             content:"\\00a0\\00a0\\00a0\\00a0\\00a0";
             text-decoration:line-through;
         }}
-        
+
         body {{
             padding: 0;
             margin: 0;
@@ -139,42 +128,15 @@ def create_image(user_name, user_avatar, message_content):
             </html>
         '''
 
-    img_dir = f"{dir_path}/quote_generator_helper"
-    img_path = f"{img_dir}/picture.png"
-    hti = Html2Image(size=(644, 264), output_path=img_dir)
-    try:
-        hti.screenshot(html_str=html, save_as="picture.png")
-        # imgkit.from_string(html, img_path, options=options)  # , config=config)
-    except OSError:
-        raise OSError("\n\nYou need to install wkhtmltoimage. Go to https://wkhtmltopdf.org/downloads.html and place \n"
-                      "the binary somewhere that `which wkhtmltoimage` (Linux) or `where wkhtmltoimage` (Windows) \n"
-                      "can find it (you may need to add it to your system path).")
+    img_path = f"{dir_path}/picture.png"
+    imgkit.from_string(html, img_path, options=options)
     return img_path
 
-
 # for testing
-# test speed
-
-start = time.time()
-
-image = 'https://www.citizen.co.za/rekord/wp-content/uploads/sites/85/2022/08/Rekord-LL-image-780x470.jpg'
-
-
-user_avatar = Image.open(requests.get(image, stream=True).raw).convert('L')
-user_avatar_path = f"{dir_path}/quote_generator_helper/user_avatar_grayscale.png"
-user_avatar.save(user_avatar_path)
-
-create_image('Priúñaku',
-             user_avatar_path,
-             'Hello from the other side')
-
-end = time.time()
-
-# do a formatting print in ms
-
-print(f"Time taken: {(end - start) * 1000} ms")
-
-
+if __name__ == "__main__":  
+    create_image('Priúñaku',
+                'https://cdn.discordapp.com/avatars/166580565548466176/a106c3ab56c9c99d48b437b05a5552e4.png?size=256',
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel ultricies ultricies')
 
 
 
