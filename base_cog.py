2f437dc47458e0c150eb65927f049d47e40206bb
Jaleel-VS <jeremikress@gmail.com>
2024-03-09 14:20:47 +0200

    Add Bot import to base_cog.py

--- base_cog.py
+++ base_cog.py
@@ -1,4 +1,5 @@
 from discord.ext.commands import Cog
+from discord.ext.commands import Bot
 
 COLORS = [0x57F287, 0xED4245, 0xEB459E, 0xFEE75C, 0xf47fff, 0x7289da, 0xe74c3c,
           0xe67e22, 0xf1c40f, 0xe91e63, 0x9b59b6,
@@ -7,7 +8,7 @@ COLORS = [0x57F287, 0xED4245, 0xEB459E, 0xFEE75C, 0xf47fff, 0x7289da, 0xe74c3c,
 class BaseCog(Cog):
     """Base class for all cogs"""
     def __init__(self, bot):
-        self.bot = bot
+        self.bot: Bot = bot
 
     @staticmethod
     async def cog_command_error(ctx, error):
